67c7211a06c6fb9a09e13c2b18e1220022fdf91f
saad9alvi <156343613+saad9alvi@users.noreply.github.com>
2024-01-18 21:59:26 +0500

    Add files via upload

---
+++ Areeb_midterm.py
@@ -0,0 +1,35 @@
+#Areeb Alvi
+#Midterm Exam
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+num1=5
+print("The First Number: ",num1)
+num2=6
+print("The Second Number: ",num2)
+print("The sum of Number1,2: ",num1+num2)
+num3=25
+print("The dividend for division: ",num3)
+num4=5
+print("The divisor for division: ",num4)
+print("The Answer: ",num3/num4)
+
+#Q2 - Write a Python program to find the area of a triangle
+lotbott=10
+print("The length of the base of the triangle: ",lotbott)
+Thott=15
+print("The the height of the triangle: ",Thott)
+print("The Area of The Triangle: ",1/2*lotbott*Thott)
+
+#Q4 - Write a Python program that return all datatypes that we discussed in the class.
+integer=19
+print("The Data Type of",integer,"is",type(integer))
+float=16.5
+print("The Data Type of",float,"is",type(float))
+string="Areeb Alvi"
+print("The Data Type of",string,"is",type(string))
+boolean= 5==5
+print("The Data Type of",boolean,"is",type(boolean))
+
+#Q3 - Write a Python program to convert Celsius to Fahrenheit.
+Tic=37
+print("The temperature in celsius: ",Tic)
+print("Conversion of celsius to fahrenhiet by applying formula: ",(9/5*Tic)+32)
